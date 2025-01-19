2a3658029c81312d38355521ba2817fc9558285d
DESKTOP-21NJK29\HirayamaKenta <hirayamakenta0511@gmail.com>
2025-01-19 14:34:52 +0900

    testブランチでの変更1

--- shrimp/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ shrimp/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -10,61 +10,62 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function create()
-    {
-      // Versionの確認1_2025/01/18 14:01
-      // Versionの確認2_2025/01/18 14:40
-      // Versionの確認3_2025/01/18 14:40
-      // Versionの確認4_2025/01/19 13:41
-      // Versionの確認5_2025/01/19 13:48
-      // Versionの確認6_2025/01/19 13:49
-        return view('auth.login');
+  /**
+   * Display the login view.
+   *
+   * @return \Illuminate\View\View
+   */
+  public function create()
+  {
+    // Versionの確認1_2025/01/18 14:01
+    // Versionの確認2_2025/01/18 14:40
+    // Versionの確認3_2025/01/18 14:40
+    // Versionの確認4_2025/01/19 13:41
+    // Versionの確認5_2025/01/19 13:48
+    // Versionの確認6_2025/01/19 13:49
+    // testブランチでの変更
+    return view('auth.login');
+  }
+
+  /**
+   * Handle an incoming authentication request.
+   *
+   * @param  \App\Http\Requests\Auth\LoginRequest  $request
+   * @return \Illuminate\Http\RedirectResponse
+   */
+  public function store(LoginRequest $request)
+  {
+    // ブランチテスト1
+    // ブランチテスト2
+    // ブランチテスト3
+    // ↓パスコード認証追加分
+    if ($request->passcode !== "05110511") {
+      return redirect()->back()->with("message", "passcodeは制作者に提示されたものを打ち込んでください。");
     }
-
-    /**
-     * Handle an incoming authentication request.
-     *
-     * @param  \App\Http\Requests\Auth\LoginRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(LoginRequest $request)
-    {
-      // ブランチテスト1
-      // ブランチテスト2
-      // ブランチテスト3
-        // ↓パスコード認証追加分
-        if ($request->passcode !== "05110511") {
-            return redirect()->back()->with("message", "passcodeは制作者に提示されたものを打ち込んでください。");
-        }
-        // ↑パスコード認証追加分
+    // ↑パスコード認証追加分
 
 
-        $request->authenticate();
+    $request->authenticate();
 
-        $request->session()->regenerate();
+    $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
-    }
+    return redirect()->intended(RouteServiceProvider::HOME);
+  }
 
-    /**
-     * Destroy an authenticated session.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(Request $request)
-    {
-        Auth::guard('web')->logout();
+  /**
+   * Destroy an authenticated session.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\RedirectResponse
+   */
+  public function destroy(Request $request)
+  {
+    Auth::guard('web')->logout();
 
-        $request->session()->invalidate();
+    $request->session()->invalidate();
 
-        $request->session()->regenerateToken();
+    $request->session()->regenerateToken();
 
-        return redirect('/');
-    }
+    return redirect('/');
+  }
 }
